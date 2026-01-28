4877c372fdd15fe9b432fad43eded065707faadd
grz3chotnik <krzysztofkubinski2004@gmail.com>
2026-01-27 23:52:09 -0300

    - payload blob storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,6 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
-import {lexicalEditor} from "@payloadcms/richtext-lexical";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
@@ -17,20 +17,20 @@ import { Config } from "@/payload-types";
 import { Orders } from "@/collections/Orders";
 import { Reviews } from "@/collections/Reviews";
 import { isSuperAdmin } from "@/lib/access";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
-
     user: Users.slug,
     importMap: {
       baseDir: path.resolve(dirname),
     },
     components: {
-      beforeNavLinks: ["@/components/stripe-verify#StripeVerify"]
-    }
+      beforeNavLinks: ["@/components/stripe-verify#StripeVerify"],
+    },
   },
   collections: [
     Users,
@@ -61,6 +61,13 @@ export default buildConfig({
         includeDefaultField: false,
       },
       userHasAccessToAllTenants: (user) => isSuperAdmin(user),
+    }),
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
     }),
   ],
 });
